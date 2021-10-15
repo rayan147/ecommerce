aa2deebca4e22a0c4237ebeac2775cc25e6ddf44
Rayan Ramirez <rayan361@gmail.com>
2021-10-14 22:13:34 -0400

    added multi-stage-docker

--- frontend/src/components/view/Message.js
+++ frontend/src/components/view/Message.js
@@ -1,19 +1,10 @@
 
-import {memo,useRef} from 'react'
 
-import PropTypes from 'prop-types'
 
-import { makeStyles } from '@material-ui/core/styles';
-import { Alert, AlertTitle } from '@material-ui/lab';
 
-// const useStyles = makeStyles((theme) => ({
-//   root: {
-//     width: '100%',
-//     '& > * + *': {
-//       marginTop: theme.spacing(2),
-//     },
-//   },
-// }));
+import { Alert} from '@material-ui/lab';
+
+
 
 const Message = ({severity ,children}) => {
     //  const classes = useStyles()
