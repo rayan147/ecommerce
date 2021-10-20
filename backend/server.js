5609d6927762be565fb29967bc305e999cd0249d
Rayan Ramirez <rayan361@gmail.com>
2021-10-20 02:51:56 -0400

    added redis

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ import {promisify} from 'util'
 // Create and configure a Redis client.
 const redisClient = Redis.createClient({
   port:6379, 
-  host:process.env.REDIS_SERVER_IP})
+  host:'192.168.0.11'})
 redisClient.on('error', error =>  console.error(error))
 const redisSet = promisify(redisClient.set).bind(redisClient)
 const redisGet = promisify(redisClient.get).bind(redisClient)
