79b3993d70e1cda76ae7e0f843dcef4ecd74a8c4
Rayan Ramirez <rayan361@gmail.com>
2021-10-20 21:28:30 -0400

    added cache.js

--- backend/controllers/productControllers/getTopProducts.js
+++ backend/controllers/productControllers/getTopProducts.js
@@ -1,14 +1,23 @@
 import Product from '../../models/productModel.js';
 import asyncHandler from "express-async-handler"
 
+import {get,saveWithTtl} from '../../cache'
 
 
 // @desc    Get top rated products
 // @route   GET /api/products/top
 // @access  Public
 const getTopProducts = asyncHandler(async (req, res) => { 
-  const products = await Product.find({}).sort({ rating: -1 }).limit(15)
+  const cacheKey = 'getTopProducts'
+  const cachedData = await get(cacheKey)
+  
+  if(cachedData){
+    console.log('data from cached data')
+    res.json(cachedData)
+  }
 
+  const products = await Product.find({}).sort({ rating: -1 }).limit(15)
+  await saveWithTtl(cacheKey,products,300)
     res.json(products)
   })
 
