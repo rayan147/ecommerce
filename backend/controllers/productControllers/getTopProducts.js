20e1a236480429defa160b9bc84b69d5ccf00b7d
Rayan Ramirez <rayan361@gmail.com>
2021-10-20 20:41:55 -0400

    added cache.js

--- backend/controllers/productControllers/getTopProducts.js
+++ backend/controllers/productControllers/getTopProducts.js
@@ -1,24 +1,14 @@
 import Product from '../../models/productModel.js';
 import asyncHandler from "express-async-handler"
-import {promisify} from 'util'
 
-const redisSet = promisify(redisClient.set).bind(redisClient)
-const redisGet = promisify(redisClient.get).bind(redisClient)
 
-const DEFAULT_EXPIRATION = 3600
 
 // @desc    Get top rated products
 // @route   GET /api/products/top
 // @access  Public
 const getTopProducts = asyncHandler(async (req, res) => { 
   const products = await Product.find({}).sort({ rating: -1 }).limit(15)
-    try {
-       await redisSet('tops',products )
-    } catch (error) {
-      
-    }
-   
-  
+
     res.json(products)
   })
 
