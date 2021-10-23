bb4aa0efd3b2c891483d974314c2219c8ede8d88
Rayan Ramirez <rayan361@gmail.com>
2021-10-23 14:12:28 -0400

    added multer route

--- backend/test/app.js
+++ backend/test/app.js
@@ -57,29 +57,17 @@ app.use('/api/products', productRoutes);
 app.use('/api/auth/users', adminRoutes);
 app.use('/api/users',userRoutes); 
 app.use('/api/orders',orderRoutes);
-// app.use('/api/upload',uploadRoutes);
+app.use('/api/upload',uploadRoutes);
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
 
 const __dirname = path.resolve()
-const upload = multer({
-    dest: express.static(path.join(__dirname, '../uploads'))
-});
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')))
 
 //ERROR HANDLERS
 app.use(errorHandler)
 app.use(notFound)
 
-//STATIC FOLDER
 
-app.post('/api/upload', upload.single('image'), (req, res) => {
-  try {
-    console.log(req.file);
-    res.send(req.file);
-  } catch (error) {
-    console.log(error);
-  }
-    
-});
 
 return app
 }
