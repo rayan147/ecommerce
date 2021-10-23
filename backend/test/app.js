89e2cf978c2b8178eae32128069d7f175c23db8a
Rayan Ramirez <rayan361@gmail.com>
2021-10-23 15:10:08 -0400

    added uploadS3

--- backend/test/app.js
+++ backend/test/app.js
@@ -57,7 +57,7 @@ app.use('/api/products', productRoutes);
 app.use('/api/auth/users', adminRoutes);
 app.use('/api/users',userRoutes); 
 app.use('/api/orders',orderRoutes);
-// app.use('/api/upload',uploadRoutes);
+app.use('/api/upload',uploadRoutes);
 app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
 
 const __dirname = path.resolve()
