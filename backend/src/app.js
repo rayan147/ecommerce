1b0fd46c4c9e03f0b1482c241a7846d105ef290f
Rayan Ramirez <rayan361@gmail.com>
2021-11-30 14:22:28 -0500

    changed port 9080 to 80 for nginx_proxy

--- backend/src/app.js
+++ backend/src/app.js
@@ -38,11 +38,11 @@ export default function app() {
   app.use(cors());
 
   // PRODUCTIONS ROUTES
-  app.use('/api/products', productRoutes);
-  app.use('/api/auth/users', adminRoutes);
-  app.use('/api/users', userRoutes);
-  app.use('/api/orders', orderRoutes);
-  app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
+  app.use('/products', productRoutes);
+  app.use('/auth/users', adminRoutes);
+  app.use('/users', userRoutes);
+  app.use('/orders', orderRoutes);
+  app.get('/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
 
   // ERROR HANDLERS
   app.use(errorHandler);
