505654aeac18fcf164a5c94939e6abdcb371f057
Rayan Ramirez <rayan361@gmail.com>
2021-12-26 14:25:50 -0500

    FIXED ENVS VARS

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import app from './src/app.js';
 
 db().connectMongo(process.env.MONGO_URL_PROD);
 console.log('process.env.JWT_TOKEN_SECRET: ', process.env.JWT_TOKEN_SECRET);
+console.log('PORT: ', process.env.PORT);
 const PORT = process.env.PORT || 5000;
 const serverApp = app();
 const server = serverApp.listen(PORT, () =>
